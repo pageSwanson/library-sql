ad9b94140a578f95034591a06e3bb093ba1077a7
K Swa <kpswanson@gatech.edu>
2021-07-29 16:54:39 -0400

    updates to web server and notes

--- serving/webserver.ts
+++ serving/webserver.ts
@@ -14,30 +14,40 @@ for await (const conn of server) {
     // Each request sent over the HTTP connection will be yielded as an async
     // iterator from the HTTP connection.
     for await (const requestEvent of httpConn) {
+        // hanlde the basic request for loading the index
         if (requestEvent.request.url == url) {
             // The native HTTP server uses the web standard `Request` and `Response`
             // objects.
 
-            // Read the index.html file and serve it
-            const html_file = await Deno.readFile('public/index.html');
+            // Read the index.html file
+            const index_content = await Deno.readFile('public/index.html');
 
-            // create an decoder which decodes your html file to strings before sending
+            // ...  and serve it!
             // The requestEvent's `.respondWith()` method is how we send the response
             // back to the client.
             requestEvent.respondWith(
-                new Response(html_file, {
+                new Response(index_content, {
                     status: 200,
                 }),
             );
+            continue;
         }
+        // if the request is to a url that matches 'http://localhost:8080/api/data'
+        // the following code will execute
         if (requestEvent.request.url == url + 'api/data') {
-            console.log('hey I got your message!');
-            const body = `Get your data shortly`;
+            // add a console entry to debug
+            console.log('You requested data');
+
+            // store our data in the response 'body'
+            const body = `Data will be here`;
+
+            // respond to the request with the requested data
             requestEvent.respondWith(
                 new Response(body, {
                     status: 200,
                 }),
             );
+            continue;
         }
     }
   })();
